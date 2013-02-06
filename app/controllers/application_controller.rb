8555934fcd44aab5a02f45ff28b83fe67924f53e
Olivier Zeller <o.zeller@gmail.com>
2013-02-06 20:38:15 +0100

    Finish sign in-out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  #to have access to the SessionsHelper in the SessionController
+  #(and not just in the sessions/views)
+  include SessionsHelper
 end
